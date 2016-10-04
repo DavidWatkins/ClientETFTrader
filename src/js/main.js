92b3379880a694f47fefc592467c9a35b792fe63
David Watkins <djw2146@columbia.edu>
2016-10-03 21:17:46 -0400

    Working version of interface

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,25 @@
 'use strict';
 
-angular.module('controllerAsExample', [])
-  .controller('SettingsController1', SettingsController1);
+var mainApp = angular.module("mainApp", []);
 
-function SettingsController1() {
-  this.name = 'John Smith';
-  this.contacts = [
-    {type: 'phone', value: '408 555 1212'},
-    {type: 'email', value: 'john.smith@example.org'}
+mainApp.controller('etfTraderController', ['$scope', function($scope) {
+  $scope.trades = [
+    {"time": "Now",
+      "quantity": 1000,
+      "order": "SELL",
+      "cost":127.10
+     }
   ];
-}
 
-SettingsController1.prototype.greet = function() {
-  alert(this.name);
-};
+  $scope.addTrade = function() {
+    $scope.trades.push({
+      "time": "NOW",
+      "quantity": Math.floor(Math.random() * 100 + 100),
+      "order": "SELL",
+      "cost": Math.random() * 200 + 50
+    });
 
-SettingsController1.prototype.addContact = function() {
-  this.contacts.push({type: 'email', value: 'yourname@example.org'});
-};
-
-SettingsController1.prototype.removeContact = function(contactToRemove) {
- var index = this.contacts.indexOf(contactToRemove);
-  this.contacts.splice(index, 1);
-};
-
-SettingsController1.prototype.clearContact = function(contact) {
-  contact.type = 'phone';
-  contact.value = '';
-};
+    //service.saveTrades($scope.trades);
+    //$scope.trades = service.getTrades();
+  };
+}]);
