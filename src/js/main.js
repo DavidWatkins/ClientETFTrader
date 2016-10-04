2692ec942a94cab73751837466b3f7fa9086df61
David Watkins <djw2146@columbia.edu>
2016-10-03 21:39:51 -0400

    Add function version of use strict

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,27 @@
-'use strict';
+(function () {
+  'use strict';
 
-var mainApp = angular.module("mainApp", []);
+  var mainApp = angular.module("mainApp", []);
 
-mainApp.controller('etfTraderController', ['$scope', function($scope) {
-  $scope.trades = [
-    {"time": "Now",
-      "quantity": 1000,
-      "order": "SELL",
-      "cost":127.10
-     }
-  ];
+  mainApp.controller('etfTraderController', ['$scope', function($scope) {
+    $scope.trades = [
+      {"time": "Now",
+        "quantity": 1000,
+        "order": "SELL",
+        "cost":127.10
+       }
+    ];
 
-  $scope.addTrade = function() {
-    $scope.trades.push({
-      "time": "NOW",
-      "quantity": Math.floor(Math.random() * 100 + 100),
-      "order": "SELL",
-      "cost": Math.random() * 200 + 50
-    });
+    $scope.addTrade = function() {
+      $scope.trades.push({
+        "time": "NOW",
+        "quantity": Math.floor(Math.random() * 100 + 100),
+        "order": "SELL",
+        "cost": Math.random() * 200 + 50
+      });
 
-    //service.saveTrades($scope.trades);
-    //$scope.trades = service.getTrades();
-  };
-}]);
+      //service.saveTrades($scope.trades);
+      //$scope.trades = service.getTrades();
+    };
+  }]);
+})();
